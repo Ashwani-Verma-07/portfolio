875f8805f8b4fff074e7bfe23673cf701a09c1ca
Ashwani Verma <vashwani0024@gmail.com>
2022-09-01 12:48:47 +0530

    added hover video-play

--- script.js
+++ script.js
@@ -14,4 +14,29 @@ function reveal() {
   }
 }
 
+// Listening to the video element
+const clip = document.querySelectorAll(".file,.file-1");
+for (let i = 0; i < clip.length; i++) {
+  /* Adding the event listeners on the video to play/pause the video. */
+  clip[i].addEventListener("mouseover", function (e) {
+    clip[i].play();
+  });
+
+  /* Applying the mouse out event to pause the video */
+  clip[i].addEventListener("mouseout", function (e) {
+    clip[i].pause();
+  });
+}
+// const clip_1 = document.querySelectorAll(".file-1");
+// for (let i = 0; i < clip_1.length; i++) {
+//   /* Adding the event listeners on the video to play/pause the video. */
+//   clip_1[i].addEventListener("mouseover", function (e) {
+//     clip_1[i].play();
+//   });
+
+//   /* Applying the mouse out event to pause the video */
+//   clip_1[i].addEventListener("mouseout", function (e) {
+//     clip_1[i].pause();
+//   });
+// }
 window.addEventListener("scroll", reveal);
